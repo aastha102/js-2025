cb3deed6a49b1ea16b7ca21ab5fbb881f0f4fcf1
aastha102 <aastha09c@gmail.com>
2024-12-03 17:38:27 +0000

    for_each_loop

--- 05_iterations/04_for_each_loop.js
+++ 05_iterations/04_for_each_loop.js
@@ -1,19 +1,26 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
+// forEach loop with callback function - no name function,  val represents every values of arra,function used in array
+
 // coding.forEach( function (val){
 //     console.log(val);
 // } )
 
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
+// forEach with arrow function 
 
-// function printMe(item){
-//     console.log(item);
-// }
+coding.forEach( (item) => {
+    console.log(item);
+} )
+ 
+// function reference passed as parameter to ForEach loop
 
-// coding.forEach(printMe)
+function printMe(item){
+    console.log(item);
+}
 
+coding.forEach(printMe) 
+
+ // we can pass all this as parameter to forEach 
 // coding.forEach( (item, index, arr)=> {
 //     console.log(item, index, arr);
 // } )
@@ -36,4 +43,18 @@ const myCoding = [
 myCoding.forEach( (item) => {
     
     console.log(item.languageName);
+    console.log(item.languageFileName);
+} )
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+    console.log(item)
+
+    // here item represents every item of array
+    //3 objects inside array
+    // backstick used for value with string
+
+    console.log(` Language name is ${item.languageName}`)
 } )
